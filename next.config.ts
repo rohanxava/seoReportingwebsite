c1589a0ee6331cfcc3db606662a24dc768e543c9
Rohan Goyal <rohangoyal8897@gmail.com>
2025-08-05 06:23:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'xava.co.in',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
